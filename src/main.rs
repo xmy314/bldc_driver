92fc627b8bc87293bac3c6e3926a747314c5ae60
xmy314 <kaishangxmy@gmail.com>
2024-07-28 14:08:39 -0700

    feat: working minimal BLDC openloop driver
    Implemented a small open loop 3PWM BLDC driver.
    Wrote a short test and verified that the driver works.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
 
+use core::cmp;
+use core::cmp::Ordering;
+
+use bldc::BLDCDriver;
 // some debug stuff
 use cortex_m_rt::entry;
 use defmt::*;
@@ -17,7 +21,7 @@ use defmt_rtt as _;
 use panic_probe as _;
 
 // the shared library of all embeded systems.
-use embedded_hal::digital::OutputPin;
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
 // specify the board
 use rp2040_hal as hal;
 // access the hardware
@@ -27,6 +31,7 @@ use hal::{
 
 // other drivers
 use as5600::As5600;
+mod bldc;
 
 #[entry]
 fn main() -> ! {
@@ -62,42 +67,60 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // setup i2c
-    // Configure two pins as being I²C, not GPIO
-    let sda_pin = pins.gpio0.reconfigure();
-    let scl_pin = pins.gpio1.reconfigure();
-    // let not_an_scl_pin: Pin<_, FunctionI2C, PullUp> = pins.gpio20.reconfigure();
+    // Init PWMs
+    let mut pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
 
-    // Create the I²C drive, using the two pre-configured pins. This will fail
-    // at compile time if the pins are in the wrong mode, or if this I²C
-    // peripheral isn't available on these pins!
-    let i2c = hal::I2C::i2c0(
-        pac.I2C0,
-        sda_pin,
-        scl_pin, // Try `not_an_scl_pin` here
-        400.kHz(),
-        &mut pac.RESETS,
-        &clocks.system_clock,
-    );
+    // Configure PWM slices
+    let pwm0 = &mut pwm_slices.pwm0;
+    pwm0.clr_ph_correct();
+    pwm0.set_top(0x0fff);
+    pwm0.enable();
+    let pwm1 = &mut pwm_slices.pwm1;
+    pwm1.clr_ph_correct();
+    pwm1.set_top(0x0fff);
+    pwm1.enable();
 
-    let mut as5600 = As5600::new(i2c);
-    // wait a little for setting up?
-    delay.delay_ms(2000);
+    // get PWM channels
+    let channel0a = &mut pwm0.channel_a;
+    let channel0b = &mut pwm0.channel_b;
+    let channel1a = &mut pwm1.channel_a;
 
-    let agc = as5600.automatic_gain_control().unwrap();
-    let mag = as5600.magnitude().unwrap();
-    let zmco = as5600.zmco().unwrap();
+    // set the pwm channels to pins
+    channel0a.output_to(pins.gpio16);
+    channel0b.output_to(pins.gpio17);
+    channel1a.output_to(pins.gpio18);
 
-    info!("{:?}", agc);
-    info!("{:?}", mag);
-    info!("{:?}", zmco);
+    // put in the motor specifications, not used for now.
+    let motor_specification = bldc::BLDCMotor {
+        pole_pairs: 4,
+        kv: 1000,
+        phase_resistance: 0.1667,
+    };
 
-    delay.delay_ms(2000);
-    info!("Hello, World");
+    // give the pwm channels to a motor driver.
+    let mut motor_driver = bldc::BLDCDriver3PWM {
+        motor_specification: motor_specification,
+        a: channel0a,
+        b: channel0b,
+        c: channel1a,
+    };
+
+    let mut led_pin = pins.gpio15.into_push_pull_output();
+
+    let mut toggle = true;
+
+    let mut phase: u16 = 0;
     loop {
-        let value = as5600.angle().unwrap();
-        println!("{:?}", value);
+        toggle = !toggle;
+        if toggle {
+            led_pin.set_high().unwrap();
+        } else {
+            led_pin.set_low().unwrap();
+        }
 
-        delay.delay_ms(10); // Don't comment out this line or RTT blows up
+        motor_driver.set_phase(phase);
+        phase = (phase + 1) % 360;
+
+        delay.delay_us(100); // Don't comment out this line or RTT blows up
     }
 }
